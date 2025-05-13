de886ad158e430f22c16c72c84430a6de2909e89
HS-Sloan <sloanlima@gmail.com>
2025-05-13 20:18:44 -0300

    Delete CommentService.java

--- CommentService.java
+++
@@ -1,8 +0,0 @@
-package Tiktok;
-
-public class CommentService {
-	public void addComment(Video video, String comment) {
-		video.addComment(comment);
-		System.out.println("Comentário mencionado ao video " + video.getTitle() + ": " + comment);
-	}
-}

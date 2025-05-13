d88de7229816842c1dc1ae65f85730da716d41a3
HS-Sloan <sloanlima@gmail.com>
2025-05-13 20:18:14 -0300

    Delete LikeService.java

--- LikeService.java
+++
@@ -1,8 +0,0 @@
-package Tiktok;
-
-public class LikeService {
-	public static void addLike(Video video, User user) {
-		video.addLike(user);
-		System.out.println(user.getName() + " achou massa esse video de " + video.getTitle());
-	}
-}
